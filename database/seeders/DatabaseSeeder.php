fb4134fbb8a203e02a076e547da979757de5e1ba
giedkon <giedkon@gmail.com>
2022-07-19 19:11:40 +0300

    Seeder Cleanup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,6 @@ class DatabaseSeeder extends Seeder
             DepartmentSeeder::class,
             StatusSeeder::class,
             CarSeeder::class,
-            //CarStatusSeeder::class,
-            //CarManagementSeeder::class
         ]);
     }
 }
